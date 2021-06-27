fc83b0eac485bcbe5a0de1510a4e415628ef92e5
naokonakanish <fu2fuka.nao@gmail.com>
2021-06-27 15:48:37 +0900

    export 'default' not found in './routes'

--- app/app.js
+++ app/app.js
@@ -1,7 +1,13 @@
 import Vue from 'nativescript-vue'
 
+// NativeScript-Vue-Natigator https://market.nativescript.org/plugins/nativescript-vue-navigator/
+import Navigator from 'nativescript-vue-navigator'
+import routes from './routes'
+Vue.use(Navigator,{routes})
+
 import Home from './components/Home'
+import RecipeDetail from './components/RecipeDetail'
 
 new Vue({
-  render: (h) => h('frame', [h(Home)]),
+  render: (h) => h(App), // default [render: 'h => h('frame', App),'] changed for Navigator
 }).$start()
---
+++ app/routes.js
@@ -0,0 +1,13 @@
+// NativeScript-Vue-Navigator用
+
+import Home from './components/Home'
+import RecipeDetail from './components/RecipeDetail'
+
+export const routes = {
+  '/home': {
+    component: Home,
+  },
+  '/recipedetail': {
+    component: RecipeDetail,
+  },
+}
