82f010359aebeaf08e0c3dd86ebbd3eac437b573
naokonakanish <fu2fuka.nao@gmail.com>
2021-06-30 21:24:05 +0900

    Action Bar changed

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <Page>
-        <ActionBar>
-            <Label text="Home"/>
+        <ActionBar title="nm-recipes">
+            <Label text="nm-recipes" fontSize="24" verticalAlignment="center" />
         </ActionBar>
 
         <GridLayout>
