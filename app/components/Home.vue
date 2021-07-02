df1e6bac0168547571f1ce6eb12fe58fb9e0216d
naokonakanish <fu2fuka.nao@gmail.com>
2021-07-02 12:54:35 +0900

    Play GridLayout

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -6,11 +6,16 @@
 
         </ActionBar>
 
-        <GridLayout>
-            <Label class="info">
+        <GridLayout columns="*" rows="100, 300" backgroundColor="#e0f5cb">
+            <Label class="info" row="0" col="0" backgroundColor="#ffffff">
                 <FormattedString>
                     <Span class="fas" text.decode="&#xf135; "/>
                     <Span :text="message"/>
+                </FormattedString>
+            </Label>
+            <Label class="info" row="1" col="0" backgroundColor="#95b576">
+                <FormattedString>
+                    <Span :text="message2" />
                 </FormattedString>
             </Label>
         </GridLayout>
@@ -22,6 +27,9 @@
     computed: {
       message() {
         return "Blank {N}-Vue app";
+      },
+      message2() {
+          return "test";
       }
     }
   };
@@ -36,8 +44,7 @@
     }
 
     .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
+       font-size: 20;
+       text-align: center;
     }
 </style>
