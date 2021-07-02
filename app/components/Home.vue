009f6134561d0ed70bef35ab1efd93559325de58
naokonakanish <fu2fuka.nao@gmail.com>
2021-07-02 16:36:58 +0900

    v-for on StackLayout of recipes

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -6,7 +6,7 @@
 
         </ActionBar>
         <StackLayout backgroundColor=#c9c9c3>
-            <GridLayout height="260" columns="*" rows="40, 220">
+            <GridLayout v-for="n in numOfRecipes" :key="n" height="260" columns="*" rows="40, 220">
                 <Label text="pudding" row="0" col="0" backgroundColor="#ffffff" />
                 <Image class="info" row="1" col="0" backgroundColor="#95b576" src="https://lh3.googleusercontent.com/QIBYEZbfFdfSVsWzYBgKh0ngvbsgAPlBxrfWwMDV3OtB4gxFue3gSDADwwpLV_PgajYr-SDLCxTdPQubhL1kz_G-wJBoLog3qhh65dyVB2V6dAKbFi6wUIM5tjf0M2GEEsCVsCQzojJOuZXU5oIJNTZwBToNVkGV7-LaVJyouRt_ihUJarIvA_W1MLggvDJ_3ZCgF7jFUrUjzIk514QT-omWxMdY3qPH60jZlv-TXKzdW-bxtU9pQNgh1Vkpz9sZ0ejtE-DbyIy-Y2MNob8efc9uENJYBv72JJYE5Y67HDlnaLeQjQlr65PcknyYNRW7gZ5tsN-SmMScMhIbemWBeSeCVGOPVnNFo899KIuRB4CPcoE1TAYdAg0ycvnzQJKwvjtrjLjMfjZylZySIiaZcQ5ANitN_nzFDT1KKVHlS-NkZ-N0B5a1e890QJuoiTd04_0DtdOebuzN1Dsp7oXU_fplRxpLL_QxgKElrQuTc6GPuYWmwwVhMjO5nPIoOpJiI5p3vd3iLTwcF1zPLeuJ4q3rkm-YaOn9TS_T6SzujReRoJsfVdARh_1YxlTznAMgLVVU8x-rkUtATZyx1gNn-PKEcCRWn9Jzzv90L3FMYW-Lt0XzNM4WsLxHeZE-QPYCAIp5XsiCgICrbGzb1yUzwuxNHqXlECn5qmiUYtvQnC1wFVB132uq9tG5tDJJzoAtMYleKxwKA5ARUz1nSqwxjb-ELQ=w2422-h1364-no?authuser=0" stretch="aspectFit" />
             </GridLayout>
@@ -16,6 +16,11 @@
 
 <script>
   export default {
+    data() {
+        return {
+            numOfRecipes: 3
+        }
+    },
     computed: {
       message() {
         return "category tab will come here";
