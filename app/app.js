f8e972354ab00d99e7ce7169be5bac75114000f2
naokonakanish <fu2fuka.nao@gmail.com>
2021-07-03 23:31:08 +0900

    Can't resolve 'tns-core-modules/utils/utils

--- app/app.js
+++ app/app.js
@@ -2,9 +2,11 @@ import Vue from 'nativescript-vue'
 import App from '@/App.vue'
 // NativeScript-Vue-Natigator https://market.nativescript.org/plugins/nativescript-vue-navigator/
 import Navigator from 'nativescript-vue-navigator'
+
+import MultiDrawer from 'nativescript-vue-multi-drawer'
 import {routes} from './routes'
 Vue.use(Navigator , { routes })
-
+Vue.use(MultiDrawer)
 import Home from './components/Home'
 import RecipeDetail from './components/RecipeDetail'
 
