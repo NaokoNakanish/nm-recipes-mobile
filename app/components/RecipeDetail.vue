9afefc409c9a816d2077fa25f3a8f6c2de27d264
naokonakanish <fu2fuka.nao@gmail.com>
2021-07-04 15:05:32 +0900

    Add ingredients List

--- app/components/RecipeDetail.vue
+++ app/components/RecipeDetail.vue
@@ -11,6 +11,35 @@
                 <Label text="Last update on 2020/10/2 by minoru" row="0" col="1" color="#D6C428" backgroundColor="#ffffff" />
             </GridLayout>
             <Image height="220" src="https://lh3.googleusercontent.com/pkjHuAsV-oHxh1yLhNkmRdwaVto3YOzbJdDaGqhkv28h2Jy7KwZRsLmbj9_mep_3fgPCD6kfridXEL3iaTzGmrQkcmJdmM_4CwCHckvHh4bxR7VNKI3l-fyBjVDPlNXU7srwOW-Dsnc=w2400" stretch="aspectFill" />
+            <Label text="ingredients" height="30" color="#ffffff" backgroundColor="#D6C428" />
+            <ListView for="ingredient in ingredients">
+                <v-template>
+                    <Label textWrap="true" backgroundColor="#ffffff">
+                        <Span :text="ingredient.ingredient" />
+                        <Span text="..." />
+                        <Span :text="ingredient.quantity" />
+                    </Label>
+                </v-template>
+            </ListView>
         </StackLayout>
     </page>
 </template>
+
+<script>
+  export default {
+    data() {
+        return {
+            ingredients: [
+                {
+                ingredient: "にく",
+                quantity: "3枚"
+                },
+                {
+                ingredient: "にんじん",
+                quantity : "2本"
+                }
+            ]
+        }
+    },
+  };
+</script>
