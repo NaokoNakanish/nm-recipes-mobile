8930d1793b2b65cb3bf4cccaf1a8d932005d597b
naokonakanish <fu2fuka.nao@gmail.com>
2021-07-04 15:38:54 +0900

    padding between Lists...??

--- app/components/RecipeDetail.vue
+++ app/components/RecipeDetail.vue
@@ -1,20 +1,38 @@
 <template>
     <page>
+        <!-- recipe title -->
         <ActionBar class="fab" title="RecipeDetail">
             <Label text="RecipeName" fontSize="24" verticalAlignment="center" />
             <ActionItem ios.systemIcon="7" ios.position="right" text="Compose" />
         </ActionBar>
-        
+
         <StackLayout backgroundColor=#c9c9c3>
+            <!-- category and update info -->
             <GridLayout height="40" columns="*, 3*" rows="*" backgroundColor="#D6C428">
                 <Label text="Category" row="0" col="0" color="#ffffff" backgroundColor="#D6C428" horizontalAlignment="center"/>
                 <Label text="Last update on 2020/10/2 by minoru" row="0" col="1" color="#D6C428" backgroundColor="#ffffff" />
             </GridLayout>
             <Image height="220" src="https://lh3.googleusercontent.com/pkjHuAsV-oHxh1yLhNkmRdwaVto3YOzbJdDaGqhkv28h2Jy7KwZRsLmbj9_mep_3fgPCD6kfridXEL3iaTzGmrQkcmJdmM_4CwCHckvHh4bxR7VNKI3l-fyBjVDPlNXU7srwOW-Dsnc=w2400" stretch="aspectFill" />
+            
+            <!-- ingredients List -->
             <Label text="ingredients" height="30" color="#ffffff" backgroundColor="#D6C428" />
             <ListView for="ingredient in ingredients">
                 <v-template>
                     <Label textWrap="true" backgroundColor="#ffffff">
+                        <Span text="・" />
+                        <Span :text="ingredient.ingredient" />
+                        <Span text="..." />
+                        <Span :text="ingredient.quantity" />
+                    </Label>
+                </v-template>
+            </ListView>
+            
+            <!-- Process List -->
+            <Label text="ingredients" height="30" color="#ffffff" backgroundColor="#D6C428" />
+            <ListView for="ingredient in ingredients">
+                <v-template>
+                    <Label textWrap="true" backgroundColor="#ffffff">
+                        <Span text="・" />
                         <Span :text="ingredient.ingredient" />
                         <Span text="..." />
                         <Span :text="ingredient.quantity" />
