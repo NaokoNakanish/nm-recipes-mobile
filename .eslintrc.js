2b97f5b19a897af86123d78f68a637413c893f28
naokonakanish <fu2fuka.nao@gmail.com>
2021-07-10 00:01:38 +0900

    eslint, prettier setting reset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,30 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true
   },
+
   extends: ['plugin:vue/essential', 'standard'],
+
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2020,
     sourceType: 'module'
   },
+
   plugins: ['vue'],
-  rules: {}
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+
+  root: true,
+
+  'extends': [
+    'plugin:vue/essential',
+    'standard',
+    'eslint:recommended',
+    '@vue/prettier'
+  ]
 }
