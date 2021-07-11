6aa05285934c17d91a33b27368d839375ab6770d
naokonakanish <fu2fuka.nao@gmail.com>
2021-07-11 16:44:20 +0900

    eslintrc.js follow to vuemastery instruction

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,18 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es2021: true,
     node: true,
   },
-
-  extends: ['plugin:vue/essential', 'standard'],
-
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-
-  plugins: ['vue'],
-
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-
-  root: true,
-
   extends: [
     'plugin:vue/essential',
-    'standard',
-    'eslint:recommended',
+    'plugin:eslint/recommended',
     '@vue/prettier',
   ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
 }
